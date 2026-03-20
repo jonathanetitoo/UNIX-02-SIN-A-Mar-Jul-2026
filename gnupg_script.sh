a0ef2d4c81398f2434a357c4a092afb11c15d92b
Dav1dVaca <davacagu@uide.edu.ec>
2026-03-20 18:02:11 +0000

    libs and key generation

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,4 @@
 uname -a # da informacion acerca del ordenador
 which gpg #ubicacion del binario de gpg
 gpg --version #me da la version e informacion adicional 
+gpg --full-generate-key # genero las llaves publicas y privadas 
