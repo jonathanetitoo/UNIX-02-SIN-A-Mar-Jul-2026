6d7b51215dd9ead3669ed960ec97b4a6c78b04f5
paula795 <pasimbanape@uide.edu.ec>
2026-03-20 18:01:52 +0000

    full

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,4 @@
 uname -a #Da informacion del ordenador 
 which gpg #ubicación del binario del gpg 
 gpg --version #muestra la version e info de gpg
+gpg --full-key-generate #genero llave publica y privada
