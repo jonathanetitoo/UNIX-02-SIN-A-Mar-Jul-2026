019f374e18d0acf9ad7f8d27a6b5f1a4ad675739
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-03-20 18:02:10 +0000

    libs and key generation

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,4 @@
 uname -a #daba información de la computadora u ordenador
 which gpg #muestra la ubicación del binario de gpg
 gpg --version #muestra la versión de gpg e información adicional
+gpg --full-generate-key #genero la llave publica y privada
