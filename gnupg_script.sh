783fcb199825cd4c93065d5abd4defda8c10dd4a
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-03-20 18:07:52 +0000

    export

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,5 @@ uname -a #daba información del ordenador
 wich gpg #nos indica donde esta el binario del gpg
 gpg --version # te indica la versión del gpg
 gpg --full-generate-key #genero una llave pública y privada 
+gpg --list-keys
+pg --armor --export dievelozes@uide.edu.ec> mi_llave_publica.asc
