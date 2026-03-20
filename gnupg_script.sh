2dd125812faedc0e16079cfd79f59b8a86cface1
pame1501 <patoapantapi@uide.edu.ec>
2026-03-20 18:05:49 +0000

    llave pública

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,4 @@ uname -a #daba información del ordenador
 which gpg #muestra ubicación del binario de gpg
 gpg --version #muestra la versión y verifica que todo esté instalado
 gpg --full-generate-key # genero la llave pública y privadausamos RSA 4096 para dar complejidad
+gpg --armor --export # exporta la llave pública
