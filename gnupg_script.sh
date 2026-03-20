49bea816e722e5bcec5f88224b6931c2cc67aa94
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-03-20 18:02:14 +0000

    key generation

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,4 @@
 uname -a #daba informacion del ordenador
 which gpg #nos da la ubicacion del binario de gpg
 gpg --version #me da la version e informacion adicional 
+gpg --full-generate-key #genero la llave publica y privada
