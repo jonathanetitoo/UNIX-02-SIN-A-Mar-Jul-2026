34fdce50795cb3301e74564f8dd95de60ee4fc8a
Slucina04060708 <salucinaar@uide.edu.ec>
2026-03-20 18:06:48 +0000

    Correction of definitions and spelling

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,5 +1,5 @@
 uname -a #daba información del ordenador
 which gpg #ubicación del binario de gpg
-gpg --version #me da la versión e infroamción adicional
+gpg --version #me da la versión e información adicional
 gpg --full-key-generate #genero llave pública y privada
 gpg --armor --export #exportar llave pública
