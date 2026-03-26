6891e89319b5bae437a530c0c446b0518435f4bf
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-03-26 12:37:17 +0000

    privatekey

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -3,3 +3,4 @@ which gpg #nos da la ubicacion del binario de gpg
 gpg --version #me da la version e informacion adicional 
 gpg --full-generate-key #genero la llave publica y privada
 gpg --armor --export #exportaba la llave publica a un archivo 
+gpg --list-secret-keys --keyid-format=long #helps to get the private key
