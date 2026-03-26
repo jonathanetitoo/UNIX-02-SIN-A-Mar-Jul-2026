1130b7c9e5f2cec4c113087125fbe244749278e0
josueeeRA <josueandresra2006@gmail.com>
2026-03-26 12:42:22 +0000

    Secret keys

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,6 @@ uname -a #Da información sobre el ordenador
 which gpg #Es la ubicación del binario de gpg
 gpg --version #Nos da la version e informacion adicional 
 gpg --full-generate-key #genero las llaves publicas y privadas 
+gpg --list-keys # Listar las keys 
+gpg --list-secret-keys -keyid-format=long #Listar llaves privadas
+GPG --armor --export-secret-keys XXXXXXXXX #Export secret keys 
