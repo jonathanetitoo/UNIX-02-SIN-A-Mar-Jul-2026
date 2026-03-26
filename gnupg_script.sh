8e0c5dabda0a0619026e5d9d24eeef4f35cacce7
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-03-26 12:36:15 +0000

    id keys

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -3,4 +3,5 @@ wich gpg #nos indica donde esta el binario del gpg
 gpg --version # te indica la versión del gpg
 gpg --full-generate-key #genero una llave pública y privada 
 gpg --list-keys
-pg --armor --export dievelozes@uide.edu.ec> mi_llave_publica.asc
+gpg --armor --export dave87821@gmail.com> mi_llave_publica.asc 
+gpg --list-secret-keys --keyid-format=long
