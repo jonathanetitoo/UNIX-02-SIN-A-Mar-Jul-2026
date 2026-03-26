e74790657e69f5eced5b7b65d6feff5468d10b6d
elskibidikevinxd <kevinalmache7@gmail.com>
2026-03-26 13:07:57 +0000

    comit que me olvide para ver nuestra llave publica

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -6,5 +6,6 @@ gpg --list-keys #lista las llaves
 gpg --armor --export kevinalmache7@gmail.com > mi_llave_publica.asc #exportar a archivo y formato legible
 gpg --list-secret-keys --keyid-format=long #para listar llaves privadas
 gpg --armor --export-secret-keys XXXXXXXXXXX #para exportar mis llaves privadas, debi a un problema yo use, gpg --pinentry-mode loopback --export-secret-keys --armor XXXXXXXXXXXXXXX (valor del hash)
-gpg -- mport llave:punlica.asc #lo usamos para importar la llave de nuestro amigo
+gpg --armor --export #para ver nuestra llave publica y poder mandarsela a nuestro amigo
+gpg --import llave:punlica.asc #lo usamos para importar la llave de nuestro amigo
 gpg --list-keys #verificamos que tengamos la llave de nuestro amigo
