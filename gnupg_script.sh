0cf77dae2b66bbcae4cc07baaeaafcb2d2064030
Slucina04060708 <salucinaar@uide.edu.ec>
2026-03-26 14:02:43 +0000

    Generate cleartext signature for document

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -14,5 +14,16 @@ gpg --armor --export slucina0406@gmail.com >mi_llave_pública.asc #exporto mi ll
  gpg --import PAULA tu_llave_pública.asc #Importo la llave publica de mi compañera y creo un nombre para esa llave
  echo "esta" >doc_no_cifrado.txt #echo "esta": Imprime la palabra "esta" en la terminal. >: Es un redireccionador. En lugar de mostrar el texto en pantalla, lo envía a un archivo. doc_no_cifrado.txt: Es el nombre del archivo que se creará (o se sobrescribirá si ya existe). 
 gpg --output doc_cifrado.txt --encrypt --recipient xxxxxxxx doc_no_cifrado.txt #Usa GnuPG para cifrar el archivo doc_no_cifrado.txt mediante la clave del destinatario xxxxxxxx y guarda el resultado en doc_cifrado.txt.
+
 gpg --output doc_cifrado.txt --encrypt --recipient xxxxxxxx doc_no_cifrado.txt #Usa GnuPG para cifrar el archivo doc_no_cifrado.txt mediante la clave del destinatario xxxxxxxx y guarda el resultado en doc_cifrado.txt.
 gpg --decrypt PAULA_doc_cifrado.txt # desencripta el archivo de mi compañera y asi puedo ver el mensaje (decrypt correction)
+
+gpg --decrypt PAULA_doc_cifrado.txt # desencripta el archivo de mi compañera y asi puedo ver el mensaje
+web of trust #sistema distribuido, no cnetralizado en una compañia, yo decido a quien darle la confianza para certificar otras claves
+# 1 Prefiero no decirlo
+# 2 No tengo confianza
+# 3 Confio un poco (marginal)
+# 4 Confio totalmente (full)
+# 5 Confio Absolutamente (propias claves)
+gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt # Crea una firma digital en texto claro sobre doc_no_cifrado.txt, permitiendo que el contenido sea legible pero garantizando su autenticidad e integridad en el archivo de salida.
+(clearsign)
