88fc41cdbf39d6ceb019b08529a7164b860e7937
JonathanMullo <jomullopa@uide.edu.ec>
2026-03-26 13:13:52 +0000

    msj_my_friend

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -6,4 +6,5 @@ gpg --armor --export #exportaba una llave publica a un archivo
 gpg --list-secret-keys --keyid-format=long #This code help to get the private key
 gpg --armor --export-secret-keys #This code help to export the keys secret
 gpg --armor --export jonathanmullo100@gmail.com > mi_llave_publica.asc #Export my public key
-gpg --import lucho_llave_publica.asc # #Import public key of my friend
+gpg --import lucho_llave_publica.asc #Import public key of my friend
+echo "Cfm lucho pintemonos el pelo" >msj_my_friend.txt #Save a message to my friend
