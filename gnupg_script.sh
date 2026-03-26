ea695925803d07e62ba8a704511179e59fac1b01
AreloXD2 <bybuitronva@uide.edu.ec>
2026-03-26 22:14:11 +0000

    The command is used to verify that my colleague's command is signed in binary format.

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -27,3 +27,4 @@ gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt #Add clea
 cat doc_no_cifrado_firmado.txt # verification of the created document
 gpg --verify pau_doc_no_cifrado_firmado.text.txt #verification of my colleague's document
 gpg --output doc_no_cifrado_firmado_binario.txt --sign doc_no_cifrado.txt # Sign the document in binary 
+gpg --verify pau_doc_no_cifrado_firmado_binario.text.txt #The command is used to verify that my colleague's command is signed in binary format.
