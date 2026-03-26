648278bb96d9410eb946e860cd7089fa6ea5768e
Dav1dVaca <davacagu@uide.edu.ec>
2026-03-26 13:13:57 +0000

    import friend key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,4 +5,6 @@ gpg --full-generate-key # genero las llaves publicas y privadas
 gpg --list-key # muestra todas las claves gpg publicas
 gpg --armor --export # exporta una clave publica gpg de un formato de texto
 gpg --list-secret-keys --keyid-format-long # listar las claves gpg secretas privadas 
-gpg --armor --export-secret-keys 1A2D50F7F787AE3A # exporta tu clave privada asociada al Id enviado
+gpg --armor --export-secret-keys 1A2D50F7F787AE3A # exporta tu clave privada asociada al Id enviado 
+gpg --armor --export davidsvaca07@gmail.com > mi_llave_publica.asc # exporta la llave asociada al correo
+gpg --import llavepublica.asc llave-publica.asc # importa la llave de mi compa 
