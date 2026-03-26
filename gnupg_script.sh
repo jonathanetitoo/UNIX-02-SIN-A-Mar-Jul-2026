a5ccc83dc0bb6dd3cc4dfc0d29e21afdb1566a4b
adrianGS25 <chgarciasa@uide.edu.ec>
2026-03-26 13:13:12 +0000

    Mensaje

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -6,3 +6,4 @@ gpg --full-generate-key # con esto genero la llave publica y privada
  gpg --armor --export-secret-keys 7EC2C36E917D6D0B # Exporta clave privada del ID
  gpg --armor --export adriangarciasnchez@gmail.com  > mi_llave_publica.asc # Exporta mi llave publica
  gpg --import micompa.asc  llave-publica.asc # Importa la llave de mi compa
+ echo "que te importa">doc_no_cifrado.txt # Se crea un archivo con el mensaje
