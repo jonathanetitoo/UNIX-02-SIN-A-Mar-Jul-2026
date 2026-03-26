3f64d6522c59c406c4143fc49c1211f1a1e7997d
ashweeps <asespinozaro@uide.edu.ec>
2026-03-26 12:58:10 +0000

    public

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -6,3 +6,4 @@ gpg --list-keys #Para ver las llaves
 gpg -armor --export #Para exportar las llaves 
 gpg --list-secret-keys --keyid-format=long #Para listar y ver las llaves privadas 
 gpg --armor --export-secret-keys HASH #Exporta tu clave priva y la convierte en texto para copiar
+gpg --armor --export<ashley2007egmail.com> mi_llave_publica.asc #Exporta tu clave publica 
