d219061eb8ec5d166e1a62cdd78ba67bbdc5202f
josueeeRA <josueandresra2006@gmail.com>
2026-03-26 13:33:24 +0000

    cambios

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,3 +5,7 @@ gpg --full-generate-key #genero las llaves publicas y privadas
 gpg --list-keys # Listar las keys 
 gpg --list-secret-keys -keyid-format=long #Listar llaves privadas
 GPG --armor --export-secret-keys XXXXXXXXX #Export secret keys 
+gpg --import david_llave_compa.asc  #Importar public key de mi compa 
+ $ echo "este mensaje es secreto" > doc_no_cifrado.txt #crear un archivo .txt con el texto entre comillas 
+ $ gpg --output doc_no_cifrado.txt --encrypt --recipient cristo123456 doc_no_cifrado.txt #encryptar el archivo
+ $ gpg --decrypt doc_cifrado_1.txt #Desencryptar el archivo que me mandó mi compa 
