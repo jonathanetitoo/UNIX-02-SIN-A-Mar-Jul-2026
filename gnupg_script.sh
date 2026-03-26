5e138c8f0076e504738e200e9eadbac2f7dae7db
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-03-26 13:27:33 +0000

    decrypt friend

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -9,3 +9,4 @@ gpg --armor --export luisvillarrealp125@gmail.com > mi_llave_publica.asc #export
 gpg --import jony_llave_publica.asc #import the public key of my friend
 echo "Pero si nos pintamos el pelo" > msj_my_friend.txt #create a file to save the message to my friend
 gpg --output doc_cifrado.txt --encrypt --recipient jonathanmullo100@gmail.com msj_my_friend.txt #encrypt my friend's message
+gpg --decrypt jony_doc_cifrado.txt #decrypt my friend's hidden message
