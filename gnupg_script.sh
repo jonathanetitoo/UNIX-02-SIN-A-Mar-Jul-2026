ae4b49bd6e17066bda70f353acd4cb5ea9ff93ae
elskibidikevinxd <kevinalmache7@gmail.com>
2026-03-26 13:39:01 +0000

    desencriptar el .txt

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -7,7 +7,8 @@ gpg --armor --export kevinalmache7@gmail.com > mi_llave_publica.asc #exportar a 
 gpg --list-secret-keys --keyid-format=long #para listar llaves privadas
 gpg --armor --export-secret-keys XXXXXXXXXXX #para exportar mis llaves privadas, debi a un problema yo use, gpg --pinentry-mode loopback --export-secret-keys --armor XXXXXXXXXXXXXXX (valor del hash)
 gpg --armor --export #para ver nuestra llave publica y poder mandarsela a nuestro amigo
-gpg --import llave:punlica.asc #lo usamos para importar la llave de nuestro amigo
+gpg --import llave_publica.asc #lo usamos para importar la llave de nuestro amigo
 gpg --list-keys #verificamos que tengamos la llave de nuestro amigo
 $ echo "este mensaje es secreto"> doc_no_cifrado.txt #es para crear un txt con el mensaje seleccionado, en mi caso es "este mensaje es secreto"
 gpg --output doc_cifrado.txt --encrypt --recipient kkevincruzatty@gmail.com doc_no_cifrado.txt #es para cifrar y crear un .txt para nuestro amigo, ocupamos su correo o su hash
+gpg --decrypt doc_cifrado.txt #para desencriptar el archivo de nuestro amigo, se pone el nombre del archivo
