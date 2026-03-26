4ae141a5097b564dd3e21e713ded18320b667c65
David-ui32 <cristo123456xx@gmail.com>
2026-03-26 13:20:40 +0000

    cifrado

--- class.sh
+++ class.sh
@@ -1,2 +1,3 @@
 gpg --list-secret-keys --kwyid-format=long #listar las llaves privadsa
 gpg --armor --export-secret-keys A3F0B7B744E84CA4 # exportamos la llave publica 
+gpg --output doc_cifrado.txt --encrypt --recipient josueandresra2006@gmail.com doc_no_cifrado.txt #Que cifra el archivo o el mesanej que puse ahi 
