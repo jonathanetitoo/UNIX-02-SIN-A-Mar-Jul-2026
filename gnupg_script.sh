63978d7e7f4ed229acd9d12de70730c6a78c73b1
Camila-700 <nalascanoal@uide.edu.ec>
2026-03-26 12:37:42 +0000

    secret key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -4,3 +4,4 @@ gpg --version #muestra la versión e info de gpg
 gpg --full-generate-key #genero la llave publica y privada 
 gpg --list-keys #lista las llaves q hay 
 gpg --armor --export #exporta 
+gpg --list-secret-keys --keyid-format=long #Para listar y ver las llaves privadas
