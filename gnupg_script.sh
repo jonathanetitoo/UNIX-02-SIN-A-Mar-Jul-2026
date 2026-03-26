599867d9be79183889ee800f7c2b1d7a0cdc1770
Camila-700 <nalascanoal@uide.edu.ec>
2026-03-26 13:03:19 +0000

    exp e imp

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,3 +5,6 @@ gpg --full-generate-key #genero la llave publica y privada
 gpg --list-keys #lista las llaves q hay 
 gpg --armor --export #exporta 
 gpg --list-secret-keys --keyid-format=long #Para listar y ver las llaves privadas
+gpg --armor --export-secret-keys XXXXXXXX #Exporta la clave privada y la convierte en texto para copia
+gpg --armor --export camiloscas.30.lascano@gmail.com > mi_llave_publica.asc #Exportar llave pública 
+gpg --import Ashley.asc #Importar llave pública de Ash de archivo Ashley.asc 
