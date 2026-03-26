da27ceaa2935ebf05f3a40231b19597a4b1e1786
ashweeps <asespinozaro@uide.edu.ec>
2026-03-26 12:48:35 +0000

    key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,3 +5,4 @@ gpg --full-generate-key #para generar llave pública y la llave privada
 gpg --list-keys #Para ver las llaves
 gpg -armor --export #Para exportar las llaves 
 gpg --list-secret-keys --keyid-format=long #Para listar y ver las llaves privadas 
+gpg --armor --export-secret-keys HASH #Exporta tu clave priva y la convierte en texto para copiar
