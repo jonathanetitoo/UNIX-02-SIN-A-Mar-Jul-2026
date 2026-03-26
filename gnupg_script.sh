c8d5aad361d5f357aebee34ce6e28c12e9a75cc5
Dav1dVaca <davacagu@uide.edu.ec>
2026-03-26 13:14:36 +0000

    create doc.txt

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -8,3 +8,4 @@ gpg --list-secret-keys --keyid-format-long # listar las claves gpg secretas priv
 gpg --armor --export-secret-keys 1A2D50F7F787AE3A # exporta tu clave privada asociada al Id enviado 
 gpg --armor --export davidsvaca07@gmail.com > mi_llave_publica.asc # exporta la llave asociada al correo
 gpg --import llavepublica.asc llave-publica.asc # importa la llave de mi compa 
+$ echo "rubias o morenas"> doc_no_cifrado.txt # creamos un archivo con mensaje
