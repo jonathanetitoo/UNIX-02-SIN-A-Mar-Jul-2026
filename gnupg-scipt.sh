2281320eca35950d813679704d51a02266a912d5
FlavioG_2005 <151882583+Mamador34@users.noreply.github.com>
2026-03-26 13:39:57 +0000

    decrypt

--- gnupg-scipt.sh
+++ gnupg-scipt.sh
@@ -3,3 +3,4 @@ which gpg  #ubicacion del binario de gpg
 gpg --version #me da la version del gpg e inforamcion adicional  
 gpg --full-generate-key #genero las llaves publica y privada
 gpg --armor -export # 
+gpg --list-secret-keys --keyid-format=long # lista las llaves privadas
