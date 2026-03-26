e998190aacf28641c7afe9c9e506b5bc98209444
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-03-26 13:29:32 +0000

    message from meco

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -8,3 +8,4 @@ gpg --list-secret-keys --keyid-format=long
 gpg --armor --export-secret-keys 5B89AD80508878A9
  echo "Amar es compartir">doc_no_cifrado.txt
  gpg --output doc_cifrado.txt --encrypt --recipient emilio.sebastianc@gmail.com doc_no_cifrado.txt
+ gpg --decrypt doc_cifrado.txt
