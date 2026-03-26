db630258983e636665ed3a90e86e9974df712e84
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-03-26 13:04:32 +0000

    exportpublickey

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,3 +5,5 @@ gpg --full-generate-key #genero la llave publica y privada
 gpg --armor --export #exportaba la llave publica a un archivo 
 gpg --list-secret-keys --keyid-format=long #helps to get the private key
 gpg --armor --export-secret-keys #Helps to export the private key
+gpg --armor --export luisvillarrealp125@gmail.com > mi_llave_publica.asc #export my public key 
+gpg --import jony_llave_publica.asc
