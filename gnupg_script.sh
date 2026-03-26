e83405e83ac217a98d20f3a2e3e0712235757173
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-03-26 13:18:20 +0000

    cifrado del mensaje

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -7,3 +7,4 @@ gpg --armor --export dave87821@gmail.com> mi_llave_publica.asc
 gpg --list-secret-keys --keyid-format=long
 gpg --armor --export-secret-keys 5B89AD80508878A9
  echo "Amar es compartir">doc_no_cifrado.txt
+ gpg --output doc_cifrado.txt --encrypt --recipient emilio.sebastianc@gmail.com doc_no_cifrado.txt
