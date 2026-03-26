f9435a30667c91680463480876662048c59aac3f
AreloXD2 <bybuitronva@uide.edu.ec>
2026-03-26 17:15:22 +0000

    We added comments to better organize.

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -19,4 +19,3 @@ gpg --import compa_llave_publica.asc #llave publica compa importación
 echo "este mensaje es secreto" > doc_no_cifrado.txt #mostramos el mensaje creado un archivo .txt
 gpg --output doc_cifrado.txt --encrypt --recipient paulitagaby12@gmail.com doc_no_cifrado.txt # ciframos el mensaje de mi compañero usando el correo de mi compañera o tambien puede ser el hash
 gpg --decrypt paula_doc_cifrado.text.txt #descargamos el archivo cifrado de nuestro compañero y lo desencriptamos con el comando y vemos el contenido del mensaje
-
