a7fcf4bee18a0609b85e6d53918de3d25fc39d0f
Arialejo01 <arvelascogu@uide.edu.ec>
2026-03-27 01:56:28 +0000

    libs and key generation

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,5 @@
 uname -a #It provides information about the computer on which the code is running.
 which gpg # location of the gpg binary
 gpg --version #gives the version and additional information
+gpg --full-generate-key #I generate public and private keys
+gpg --list-keys #Displays all public GPG keys in your local keyring, including user IDs, key IDs, and fingerprints
