a77944d45a298eb83ec2408da874f2b270ee80d9
pame1501 <patoapantapi@uide.edu.ec>
2026-03-27 17:10:38 +0000

    createpersonalcommand

--- fylesystem_script.sh
+++ fylesystem_script.sh
@@ -3,3 +3,8 @@ ls #list files and directories on the /dev directory
 history # carga lista los comandos realizados
 cd /proc # Real-time system and process information, directly from the terminal.
 cat cpuinfo #Display detailed information about the processor (CPU).
+#en /media se encuentra las particiones de montaje
+bin/ #encontramos archivos bonarios
+var #datos variables como archivos de BDD
+gcc saludo.c -o saludobin # crea un archivo en lenguaje c y luego este se transforma a lenguaje binario
+sudo mv saludobin /bin/ # mueve el archivo a /bin para poder ejecutarlo desde cualquier parte de la terminal
