652a2011dd789634e4c1266337afadb4143d6cbe
Camila-700 <nalascanoal@uide.edu.ec>
2026-03-27 15:37:43 +0000

    decrypt

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,11 +1,13 @@
-uname -a #Da info del ordenador 
-which gpg #ubicación del binario del gpg 
-gpg --version #muestra la versión e info de gpg 
-gpg --full-generate-key #genero la llave publica y privada 
-gpg --list-keys #lista las llaves q hay 
-gpg --armor --export #exporta 
-gpg --list-secret-keys --keyid-format=long #Para listar y ver las llaves privadas
-gpg --armor --export-secret-keys XXXXXXXX #Exporta la clave privada y la convierte en texto para copia
-gpg --armor --export camiloscas.30.lascano@gmail.com > mi_llave_publica.asc #Exportar llave pública 
-gpg --import Ashley.asc #Importar llave pública de Ash de archivo Ashley.asc 
-echo "te quiero gei">doc_no_cifrado.txt #se genera un archivo con el mensaje 
+uname -a # Gives information about the system
+which gpg # location of the gpg binary
+gpg --version # shows gpg version and info
+gpg --full-generate-key # generate public and private key
+gpg --list-keys # list available public keys
+gpg --armor --export # export key
+gpg --list-secret-keys --keyid-format=long # list and view private keys
+gpg --armor --export-secret-keys XXXXXXXX # export private key and convert it to text for copying
+gpg --armor --export camiloscas.30.lascano@gmail.com > mi_llave_publica.asc # export public key
+gpg --import Ashley.asc # import Ashley's public key from Ashley.asc file
+echo "te quiero gei" > doc_no_cifrado.txt # create a file with a message
+gpg --output doc_cifrado.txt --encrypt --recipient FF6770C528E2BF386F256480BF1F26A3D2143F6E doc_no_cifrado.txt # encrypt the file with the previously created message
+gpg --decrypt Ashley_doc_cifrado.txt # decrypt the encrypted file 
