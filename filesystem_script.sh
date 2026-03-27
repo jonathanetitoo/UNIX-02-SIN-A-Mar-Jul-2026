90d63270386412faa2ad70841672d0c3729d3c76
adrianGS25 <chgarciasa@uide.edu.ec>
2026-03-27 16:41:05 +0000

    porc directory

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -1,3 +1,6 @@
 cd /dev # change directory to /dev sirectory
 ls # list files and directories on the /dev directoey
-history # to see all the comands that you have been using
+history # to see all the comands that you had been using
+cd /proc # chande directory to /proc directory
+ls $ list files and directories on the /proc directory
+cat cpuinfo # information of the CPU
