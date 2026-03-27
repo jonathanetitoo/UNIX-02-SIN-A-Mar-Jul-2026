799f741bd221941f5a432230e83ac4fb600d0526
adrianGS25 <chgarciasa@uide.edu.ec>
2026-03-27 17:10:39 +0000

    saludobin

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -3,4 +3,8 @@ ls # list files and directories on the /dev directoey
 history # to see all the comands that you had been using
 cd /proc # chande directory to /proc directory
 ls $ list files and directories on the /proc directory
-cat cpuinfo # information of the CPU
+cat cpuinfo # information from the CPU
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/ # this one take me to the original one
+cd /etc # change directory to /etc directory
+cd 
+cd gss/ change director to gss/ directory
