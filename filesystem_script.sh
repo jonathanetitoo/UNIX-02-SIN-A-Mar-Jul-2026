20026e69aa03ef81dd2bf038a1b1a61234e32506
Camila-700 <nalascanoal@uide.edu.ec>
2026-03-27 17:12:58 +0000

    saludobin

---
+++ filesystem_script.sh
@@ -0,0 +1,9 @@
+#Change directory to /dev directory 
+cd /dev
+#List files and directories on the /dev directory 
+ls 
+#This folder contains system information and real-time processes 
+cd/proc 
+ls
+#Displays the contents of the cpuinfo file 
+cat cpuinfo 
---
+++ saludo.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+    printf("Hola mundo0n \n");
+    return 0;
+}
