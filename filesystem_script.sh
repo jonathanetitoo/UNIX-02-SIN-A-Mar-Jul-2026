dfc34d8aa6b005bc7aaffafca6c1c3172b103eb3
josueeeRA <josueandresra2006@gmail.com>
2026-03-27 16:41:12 +0000

    /proc and cat cpuinfo

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -1,3 +1,5 @@
-$cd /dev #Change directory to /dev directory 
+$cd /dev #Change directory to /dev directory, but 
 $ls #List directories on the /dev directory 
 $history #To view all the commands I run in the terminal 
+cd /proc #To change to /proc
+/proc $cat cpuinfo #Views specsof the virtual cpu
