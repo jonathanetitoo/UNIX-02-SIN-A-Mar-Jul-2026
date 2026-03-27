922f066662ea66d262926bfb5b0bf2775c109df5
paula795 <pasimbanape@uide.edu.ec>
2026-03-27 16:38:09 +0000

    To see the history of commands

---
+++ filesysten_script.sh
@@ -0,0 +1,5 @@
+#Changes the current directory to/, wich is the top-level directory in linux/Unix systems.
+cd / key
+ls
+#To see the history of commands  you have executed in the terminal.
+history
