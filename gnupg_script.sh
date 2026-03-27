a2d8eacea814b37a5c9465a380d3afd0550db10a
Arialejo01 <arvelascogu@uide.edu.ec>
2026-03-27 03:31:24 +0000

    echo

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,11 +1,15 @@
 uname -a #It provides information about the computer on which the code is running.
 which gpg # location of the gpg binary
 gpg --version #gives the version and additional information
-gpg --full-generate-key gpg --list-secret-keys --keyid-format=long#I generate public and private keys
+gpg --full-generate-key #I generate public and private keys
 gpg --list-keys #Displays all public GPG keys in your local keyring, including user IDs, key IDs, and fingerprints
 gpg --armor --export #Exports a GPG public key to an ASCII-encapsulated text format, instead of binary.
 gpg --list-secret-keys --keyid-format=long #This command is used to list the secret (private) GPG keys available in your keyring, showing their IDs in the full, long format.
 gpg --armor --export-secret-keys A472F6F558168284 #Export the secret key
-gpg --import mi_llave_pub.asc #The public key matters
-$ echo "te amo cesar"> doc_no_cifrado.txt cesar.arciniegas.mejia4@gmail.com doc_no_cifrado.txt #Create the text file
-$ gpg --output doc_cifrado.txt --encrypt --recipient cesar.arciniegas.mejia4@gmail.com doc_no_cifrado.txt #Encrypt the document
+gpg --import my_key_publicaa.asc #The public key matters
+$ #It indicates that you are operating with a standard user account, without full privileges over the system
+echo #It's the command to print text. Basically, it tells the system: "Repeat what I'm about to type."
+"love you cesar" #This is the first argument. The text you want to be processed.
+> #It's the redirection operator. Instead of displaying the result on the screen, it "pours" it into a file.
+doc_no_cifrado.txt #This is the name of the file that will receive the text. If it doesn't exist, Linux creates it; if it exists, it deletes the old file and writes the new one.
+
