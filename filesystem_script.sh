4cfb7586c5c94c46d5a7a489e398b8b48825742c
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-03-27 17:26:27 +0000

    inode

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -9,3 +9,39 @@ cd /proc
 ls 
 # gives the information of the virtual cpu 
 cpu info
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/
+
+#Change directory to the specific project folder.
+
+ls
+
+#List files and directories in the current path.
+
+gcc saludo.c -o saludobin
+
+#Compile the C file and create an executable named 'saludobin'.
+
+sudo mv saludobin /bin/
+
+#Move the executable to the system binary folder using root privileges.
+
+saludobin
+
+#Run the newly created program from the current location.
+
+cd
+
+#Return to the user's home directory.
+
+saludobin
+#Run the program again (testing if it's accessible globally via /bin/
+
+cd /etc/gss
+
+#to change directory to the GSS configuration path.
+
+history
+
+#Display the list of recently executed commands.
+ls -F
+# Los inodos de cada archovo en el directorio 
