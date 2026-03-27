7b6edd41a9d95b29d24afb3f208f996a1db4e04d
josueeeRA <josueandresra2006@gmail.com>
2026-03-27 05:16:47 +0000

    Trust, Sign Key and encryp and sign doc

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -12,5 +12,9 @@ gpg --import david_llave_compa.asc  #Importar public key de mi compa
  gpg --clearsign doc_no_cifrado.txt #Firmar el documento no cifrado pero con texto legible
 $ gpg --sign doc_no_cifrado.txt #Firmar un documento no cofrado pero con texto en binario
 $ gpg --detach-sign doc_no_cifrado.txt #Crear un documento con la firma separada .sig
-$ gpg --verify doc_no_cifrado.txt_d.sig doc_no_cifrado_David.txt #Veridicar el archivo de mi compañero
+$ gpg --verify doc_no_cifrado.txt_d.sig doc_no_cifrado_David.txt #Verificar el archivo de mi compañero
+$ gpg --edit-key 0B4FAC28129DEDA5272A486B1CEC874FFF9CB4E3 #Para poder darle el valor de trust
+$ gpg --sign-key 0B4FAC28129DEDA5272A486B1CEC874FFF9CB4E3 #Para firmar la llave de mi compañero
+$ gpg --encrypt --sign --recipient 0B4FAC28129DEDA5272A486B1CEC874FFF9CB4E3 doc_no_cifrado.txt #Enviar un archivo encyptado y firmado
+$ gpg --decrypt doc_cifrado_firmado_D.gpg #Verificar el archivo encryptado y firmado de mi compañero 
 
