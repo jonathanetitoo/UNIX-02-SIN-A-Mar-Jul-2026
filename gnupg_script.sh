b4d25a70ff0cd9634f1cef369136020e3a6574b4
Cexnvy <cearciniegasme@uide.edu.ec>
2026-03-27 01:51:55 +0000

    List Keys

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,4 +1,5 @@
-uname -a #It provided information about the computer (architecture, OS)
-which gpg #Provides the location of the GPG binary
-gpg --version #Give the version and additional information
+uname -a #It provided information about the computer (architecture, OS).
+which gpg #Provides the location of the GPG binary.
+gpg --version #Give the version and additional information.
 gpg --full-generate-key #It allows you to generate the public and private key. 
+gpg --list-keys #List the generated public keys, the User identifier, and the key expiration date.
