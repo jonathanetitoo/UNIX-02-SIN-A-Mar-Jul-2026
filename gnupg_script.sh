36acbc71250ff5eec1efa735b955ffaab71dbccd
Camila21816 <capaucargu@uide.edu.ec>
2026-03-27 23:16:30 +0000

    gpg --armor --export-secret-keys export the private key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -4,3 +4,4 @@ gpg --version #muestra la versión e información adicional
 gpg --full-generate-key #genera llave publica y privada.
 gpg --armor --export #exportaba la llave publica
 gpg --list-secret-keys --keyid-format=long # Sirve para listar las llaves privadas
+gpg --armor --export-secret-keys #sirve para exportar la llave privada
