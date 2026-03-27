fd3aa0a96c4bb0a12e409c0dea8a7603f72f1431
elskibidikevinxd <kevinalmache7@gmail.com>
2026-03-27 17:44:48 +0000

    comit incompleto

--- filesystem_scrpt.sh
+++ filesystem_scrpt.sh
@@ -5,13 +5,14 @@ ls #lost files and directories on the /proc
 cat cpuinfo #it helps to search the info of the cpu 
 cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/ #para movernos al main
 ls #buscamos lo que tenemos en el main
-gcc saludo.c  -o saludobin
-sudo mv saludobin /bin/
-saludobin
-cd /etc
-saludobin
-cd /dev
-saludobin
-cd /proc
-saludobin
-cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/
+gcc saludo.c  -o saludobin #compila saludo.c y lo hace un ejecutable llamado "saludobin"
+sudo mv saludobin /bin/ #movemos saludobin a /bin/ para poderlo ejucutar desde cualquier carpeta
+saludobin #ejecuta saludobin (lo de adentro), que printa "hola mundo"
+cd /etc #provamos moviendonos a diferentes carpetas
+saludobin #ejecutamos y printa "hola mundo" desde cualquier carpeta
+pwd
+ls -F
+cd / #moves to the root
+ls -F #list the files and directories including /(file), * (executable), @ (symbolic enlace)
+ls -i #it helps lo search the inodos (the numbers)
+
