842b2a043050bbecb87f89f662b07fba5be30a9e
Arialejo01 <arvelascogu@uide.edu.ec>
2026-03-27 01:50:40 +0000

    version

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,2 +1,3 @@
 uname -a #It provides information about the computer on which the code is running.
 which gpg # location of the gpg binary
+gpg --version #gives the version and additional information
