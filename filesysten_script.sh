200e240bd171fff0b7ae51fb69c8f8b21c91e7fe
paula795 <pasimbanape@uide.edu.ec>
2026-03-27 17:12:56 +0000

    saludobin

--- filesysten_script.sh
+++ filesysten_script.sh
@@ -3,3 +3,9 @@ cd / key
 ls
 #To see the history of commands  you have executed in the terminal.
 history
+# Go to the special /proc folder.
+#This folder contains system information and real-time processes.
+cd/proc
+ls
+# Displays the contents of the cpuinfo file.
+cat cpuinfo
