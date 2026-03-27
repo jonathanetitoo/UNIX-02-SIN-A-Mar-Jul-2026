290a3bd577cdaa984738565a5086db580282bd75
ashweeps <asespinozaro@uide.edu.ec>
2026-03-27 17:15:53 +0000

    etc wip

--- filessystem_script.sh
+++ filessystem_script.sh
@@ -9,3 +9,13 @@ cd /proc
 ls
 #Used to view detailed information about the CPU
 cat cpuinfo
+#Changes the current directory to /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/
+/proc $ cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/
+#Changes the current directory to the home directory of the logged-in user.
+ cd 
+#Executes a custom command or script named saludobin. In your context, this is probably the “greeting” script that prints a message like “Hello, welcome!”.
+saludobin
+##Changes the current directory to /etc/gss. This is likely a system configuration directory related to GSS (Generic Security Services) or other service configuration files.
+cd /etc/gss
+#Shows the list of previously executed commands in the current shell session. Useful to review what has been done or to reuse commands with !number.
+history
