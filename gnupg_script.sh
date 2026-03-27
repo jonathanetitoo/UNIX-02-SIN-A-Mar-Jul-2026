4ceff9a0f897c20b6bb01f2557d6dc9ba165563c
Arialejo01 <arvelascogu@uide.edu.ec>
2026-03-27 04:33:04 +0000

    edit key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -16,3 +16,4 @@ $ gpg --output doc_cifrado.txt --encrypt --recipient 561B9E443A91A0A7A257ACF201E
 gpg --decrypt MICOMPA_cifrado_doc.txt #With this command, I take that file that looks like digital garbage (MICOMPA_cifrado_doc.txt) and try to return it to its original state so that I can read it.
 gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt #This command is great because here I'm not hiding anything, but rather giving my digital "word of honor"!
 gpg --verify MICOMPA_doc_no_cifrado_firmado.txt #When I execute this instruction, I am not opening a sealed envelope, but rather checking the authenticity seals of a document that is perfectly legible.
+gpg --edit-key 561B9E443A91A0A7A257ACF201EE56537872D793 #When I execute this instruction, I don't just perform a single action and stop. Instead, I enter edit mode on the key that corresponds to that fingerprint. It's as if I open Caesar's entire file and wait for you to tell me what you want to change.
