8f6ae31f50ddfb1204f1464d8425f8a3ae6ed8e2
elskibidikevinxd <kevinalmache7@gmail.com>
2026-03-27 16:41:08 +0000

    cpuinfo

---
+++ filesystem_scrpt.sh
@@ -0,0 +1,5 @@
+cd /dev #change directory to /dev directory 
+ls #list files and directories on the /dev directory
+cd /proc #
+ls #lost files and directories on the /proc
+cat cpuinfo #it helps to search the info of the cpu
