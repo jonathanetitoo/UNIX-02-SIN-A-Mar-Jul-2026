0482179654b8bc5671f9d56e14006d9761015ba5
Joselyn-uide <jolagunapu@uide.edu.ec>
2026-03-27 17:19:13 +0000

    saludo

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -6,3 +6,17 @@ ls
 cd /proc
 #displays information from the cpuinfo file
 cat cpuinfo
+# Change my current working directory to the project's specific folder.
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/
+#This command uses the GCC compiler to transform the source code written in the file saludo.c into an executable program called saludobin
+gcc saludo.c -o saludobin
+# Move the saludobin executable file to the system folder /bin/
+sudo mv saludobin /bin/
+#The saludobin program is executed.
+saludobin
+#change directory to /etc
+cd /etc
+#Enter a subfolder called gss located inside /etc)
+cd gss/
+
+
