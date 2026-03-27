4bc776cec52a0a748a5dd92f48f49c29eee89089
josueeeRA <josueandresra2006@gmail.com>
2026-03-27 04:31:36 +0000

    Sing documento with clearsing and sing

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -9,3 +9,5 @@ gpg --import david_llave_compa.asc  #Importar public key de mi compa
  $ echo "este mensaje es secreto" > doc_no_cifrado.txt #crear un archivo .txt con el texto entre comillas 
  $ gpg --output doc_no_cifrado.txt --encrypt --recipient cristo123456 doc_no_cifrado.txt #encryptar el archivo
  $ gpg --decrypt doc_cifrado_1.txt #Desencryptar el archivo que me mandó mi compa 
+ gpg --clearsign doc_no_cifrado.txt #Firmar el documento no cifrado pero con texto legible
+$ gpg --sign doc_no_cifrado.txt #Firmar un documento no cofrado pero con texto en binario
