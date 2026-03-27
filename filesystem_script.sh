e879469a87436a4679227ee6f573a89b2daf09ae
JonathanMullo <jomullopa@uide.edu.ec>
2026-03-27 17:15:11 +0000

    wip: executable binary

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -8,3 +8,13 @@ history
 cd /proc
 # Displays file content on screen
 cat cpuinfo
+# Compile the file in C using GCC and generate an executable called greetingbin.
+gcc saludo.c -o saludobin
+# Move the executable to /bin/ using administrator permissions (sudo) so that the system can run it from anywhere.
+sudo mv saludobin /bin/
+# Change to the /etc directory,where system configuration files are located
+cd /etc
+# Enter the gss/ folder within the current directory
+cd gss/
+# Run the compiled program from the terminal
+saludobin
