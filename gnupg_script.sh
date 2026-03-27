690551cb01caa45f787637e98e3a043891927f92
Cexnvy <cearciniegasme@uide.edu.ec>
2026-03-27 01:40:53 +0000

    GPG Version

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,2 +1,3 @@
 uname -a #It provided information about the computer (architecture, OS)
 which gpg #Provides the location of the GPG binary
+gpg --version #Give the version and additional information
