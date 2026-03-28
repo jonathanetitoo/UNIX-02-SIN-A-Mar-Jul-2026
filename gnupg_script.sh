e6f60073d3daaf45fb517b4f550bf592631848e7
ashweeps <asespinozaro@uide.edu.ec>
2026-03-28 04:43:40 +0000

    validated encryption

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,4 +1,3 @@
-#Block A — Environment Preparation
 uname_a # Give the computer information
 which gpg # Give the location of the gpg binary
 gpg --version # It tells us the version of gpg and additional information
@@ -6,14 +5,10 @@ gpg --full-generate-key # To generate public key and private key
 gpg --list-keys #To see the keys
 gpg -armor --export # To export the keys
 gpg --list-secret-keys --keyid-format=long # To list and view the private keys
-
-#Block B — Public Key Exchange
 gpg --armor --export-secret-keys HASH # Export your private key and convert it into text to copy
 gpg --armor --export<ashley2007egmail.com> mi_llave_publica.asc #Export your public key
 gpg --import CAMILLA.asc #Export a key from a file
 echo "Tqm, Camillaaa">doc_no_cifrado.txt # The file is created and the text is redirected to a file
-
-#Block C — Encryption and decryption
 gpg --output doc_cifrado.txt --encrypt --recipient camiloscas.30.lascano@gmail.com doc_cifrado.txt # Encrypt the content and use the other person's public key
 gpg --decrypt doc_cifrado.txt # This command is used to decrypt an encrypted file.
 which gpg / gpg --version # Used to find where GPG is installed on your system.
@@ -29,7 +24,7 @@ gpg --edit-key #It allows you to edit and manage a GPG key.
  gpg --verify camilla_firma_separada_doc_no_cifrado.sig camilla_doc_no_cifrado.txt #Verify the separate signature using the original file to confirm that it is authentic and has not been modified.
  gpg --output doc_cifrado_y_firmado.txt --encrypt --sign --recipient XXXXXXX doc_no_cifrado.txt #Encrypt and sign the file, sending it securely to the indicated recipient.
  gpg --output doc_cifrado_y_firmado_descifrado_y_validado.txt --decrypt camilla_doc_cifrado_y_firmado.txt #Decrypt the file and verify the signature, saving the original content.
- cat doc_cifrado_y_firmado.txt # Displays the contents of the decrypted and verified file in the terminal.
+ cat doc_cifrado_y_firmado_y_validado.txt # Displays the contents of the decrypted and verified file in the terminal.
 
 
 
