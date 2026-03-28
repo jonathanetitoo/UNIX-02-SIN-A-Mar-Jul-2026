042d202b49fa8685dda6319f28ff159a4b9d0295
Camila21816 <capaucargu@uide.edu.ec>
2026-03-28 01:42:41 +0000

    gpg --sign doc_no_cifrado.txt sign binary

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -12,3 +12,4 @@ cat doc_no_cifrado.txt #muestra si se creo correctamente el documento
 gpg --output doc_cifrado.txt --encrypt --recipient gogosaturo57@gmail.com doc_no_cifrado.txt #toma un archivo normal y lo convierte en un archivo secreto que solo el dueño de la llave puede abrir
 gpg --decrypt doc_cifrado.txt #va a desencriptar el mensaje 
 gpg --clearsign #firmar el contenido sin ocultar el texto original
+gpg --sign doc_no_cifrado.txt # Firmar en formato binario
