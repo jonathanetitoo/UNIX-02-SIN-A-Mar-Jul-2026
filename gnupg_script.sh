07ea83e54684a3411bef4e5976bcf5e5a4cf3f66
Camila21816 <capaucargu@uide.edu.ec>
2026-03-28 00:24:20 +0000

    gpg --decrypt doc_cifrado.txt decrypt the message

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -10,3 +10,4 @@ gpg --list-keys #verifica que la importación de la llave haya sido correcta
 echo "Mensaje escrito desde mi cuenta principal" > doc_no_cifrado.txt #creacion del mensaje
 cat doc_no_cifrado.txt #muestra si se creo correctamente el documento 
 gpg --output doc_cifrado.txt --encrypt --recipient gogosaturo57@gmail.com doc_no_cifrado.txt #toma un archivo normal y lo convierte en un archivo secreto que solo el dueño de la llave puede abrir
+gpg --decrypt doc_cifrado.txt #va a desencriptar el mensaje 
