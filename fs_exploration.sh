d643289a56e301d9a859c270b2a7628b0768094f
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-04-02 13:14:51 +0000

    UEFI or bios

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -10,4 +10,6 @@ ls -F
  #Download and install the 'parted' disk partitioning tool.
  sudo parted -l && echo --e "\n---\n" && lbslk -f && echo -e
  #List all disk partitions, followed by a separator and a detailed list of block devices and file systems.
+ [ -d/sys/fimware/efi ] && echo "UEFI" || echo "BIOS"
+ #Check if the system booted via UEFI or Legacy BIOS by looking for the EFI firmware directory.
  
