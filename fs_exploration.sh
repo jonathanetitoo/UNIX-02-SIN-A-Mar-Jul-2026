086bed28d6a0f91cc8161a54f76494f0d02b4566
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-04-02 12:56:20 +0000

    supo apt instal

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -2,3 +2,7 @@ cd /
 #Change directory to the system root.
 ls -F 
 #List files and directories, adding a symbol (like / or *) to indicate file types.
+ sudo apt update 
+ sudo apt upgrade
+ sudo apt install parted 
+ sudo parted -l && echo --e "\n---\n" && lbslk -f && echo -e
