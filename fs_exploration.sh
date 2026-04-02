9ae01e29630e6209daece0c54ad37c51edce7d4c
David-ui32 <cristo123456xx@gmail.com>
2026-04-02 13:16:15 +0000

    lslk

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -7,6 +7,10 @@ sudo apt update # actualiza con exito el repositorio
 apt upgrade # actualiza 
 
 apt install parted #instalar el  paquete en partes
-sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" #Me da la informacion del sector y del disco y todos los datos 
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" #Me da la informacion del sector y del disco y todos los datos ,y espacios dispobibles 
 sudo parted -l # me da particiones 
-lsblk # 
+
+lsblk #Muestra estrucutra de discos y uuids 
+
+[ -d /sys/firmware/efi ] && echo"UEFI" || echo "BIOS    
+#muestra la bios o la uefi 
