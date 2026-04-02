fb48f66529d67eb171e1f8775c5d65f9222b8f8e
Camila-700 <nalascanoal@uide.edu.ec>
2026-04-02 13:14:58 +0000

    BIOS or UEFI

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -8,3 +8,9 @@ sudo apt upgrade
 #used to install updates for the packages already installed on your system
 sudo apt install parted
 # used to install the program parted on your Linux system
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n"
+#displays detailed information about disks and partitions (parted -l), adds separators for readability (echo), 
+#and then lists devices with their filesystems (lsblk -f) in an organized way
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS"
+#checks if the system is using UEFI by testing if the directory /sys/firmware/efi exists; 
+#if it does, it prints “UEFI”, otherwise it prints “BIOS”
