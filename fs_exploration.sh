f8c6a54a1b94f2cbfae2184377e46dbd6cfcdc94
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-04-02 13:57:43 +0000

    stat

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -9,7 +9,14 @@ ls -F
  sudo apt install parted 
  #Download and install the 'parted' disk partitioning tool.
  sudo parted -l && echo --e "\n---\n" && lbslk -f && echo -e
- #List all disk partitions, followed by a separator and a detailed list of block devices and file systems.
+ #List all disk partitions, followed by a separator and a detailed list of block devices and file systems. && (means and)
  [ -d/sys/fimware/efi ] && echo "UEFI" || echo "BIOS"
- #Check if the system booted via UEFI or Legacy BIOS by looking for the EFI firmware directory.
- 
+ #Check if the system booted via UEFI or Legacy BIOS by looking for the EFI firmware directory. ||(means or)
+echo "mi archivo" >test.txt
+#Create a new file named 'test.txt' and write the text "mi archivo" into it.
+ls 
+#List files in the current directory to verify the file was created.
+cat test.txt 
+#Display the content of the file in the terminal.
+stat test.txt
+#Show detailed status and metadata of the file (size, permissions, creation date).
