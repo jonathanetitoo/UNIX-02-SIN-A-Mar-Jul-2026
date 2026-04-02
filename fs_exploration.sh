aa4d86faabf6004eaf761c21f8826e1a030b791d
Camila-700 <nalascanoal@uide.edu.ec>
2026-04-02 13:57:50 +0000

    stat

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -10,7 +10,15 @@ sudo apt install parted
 # used to install the program parted on your Linux system
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n"
 #displays detailed information about disks and partitions (parted -l), adds separators for readability (echo), 
-#and then lists devices with their filesystems (lsblk -f) in an organized way
+#and then lists devices with their filesystems (lsblk -f) in an organized way && MEANS AND
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS"
 #checks if the system is using UEFI by testing if the directory /sys/firmware/efi exists; 
-#if it does, it prints “UEFI”, otherwise it prints “BIOS”
+#if it does, it prints “UEFI”, otherwise it prints “BIOS”. || MENAS OR 
+echo "mi archivo" > test.txt 
+#creates a file called test.txt and writes the text “mi archivo” into it
+ls
+#command lists the files and directories in the current directory
+cat test.txt 
+#displays the contents of the file test.txt in the terminal
+stat test.txt
+#shows detailed information about the file test.txt
