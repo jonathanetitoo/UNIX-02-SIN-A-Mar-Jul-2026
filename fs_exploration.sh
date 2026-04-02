6fc5f3408b8766bccc1226fbed084bcbda0fcf05
Camila-700 <nalascanoal@uide.edu.ec>
2026-04-02 12:43:16 +0000

    sudo apt update

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -2,3 +2,5 @@ cd /
 #change directory takes you to the root directory of the filesystem
 ls -F
 #list shows the contents of a directory, -F option adds a symbol at the end of each name to indicate its type
+sudo apt update 
+# to update the list of available packages
