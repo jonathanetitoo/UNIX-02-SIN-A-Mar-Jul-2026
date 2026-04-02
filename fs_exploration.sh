4a887c9b3e0065ae90f60d8ba702f90844d4608d
Kevin <132043896+kkevincruzattjw@users.noreply.github.com>
2026-04-02 12:51:25 +0000

    updates the local package list

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -3,3 +3,6 @@ Change Directory (cambiar directorio) Represents the root of the file system.
 
 ls -F
 Displays all information in alphabetical order by file name
+
+sudo apt update
+updates the local package list
