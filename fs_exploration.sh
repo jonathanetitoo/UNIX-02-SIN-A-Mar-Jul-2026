de9607b8c1b9647ebc8cb0326de5a640bf106b9c
DiegoVeloz28 <dievelozes@uide.edu.ec>
2026-04-02 13:03:56 +0000

    sudo parted

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -3,6 +3,11 @@ cd /
 ls -F 
 #List files and directories, adding a symbol (like / or *) to indicate file types.
  sudo apt update 
+ #Update the local package index with the latest metadata from repositories.
  sudo apt upgrade
+ #Install available updates for all currently installed packages.
  sudo apt install parted 
+ #Download and install the 'parted' disk partitioning tool.
  sudo parted -l && echo --e "\n---\n" && lbslk -f && echo -e
+ #List all disk partitions, followed by a separator and a detailed list of block devices and file systems.
+ 
