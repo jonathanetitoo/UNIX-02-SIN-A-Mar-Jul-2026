6ad95ab2b35860f0d0b55a6f111241814d58d829
Camila-700 <nalascanoal@uide.edu.ec>
2026-04-02 12:28:37 +0000

    ls -F

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -1,2 +1,4 @@
 cd / 
 #change directory takes you to the root directory of the filesystem
+ls -F
+#list shows the contents of a directory, -F option adds a symbol at the end of each name to indicate its type
