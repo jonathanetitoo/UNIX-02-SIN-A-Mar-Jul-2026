8d42f6ae5d66d7be0fbcb627aad238ce94b4bcfb
ashweeps <asespinozaro@uide.edu.ec>
2026-04-02 12:47:13 +0000

    installparted

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -2,3 +2,9 @@
 cd /
 # list files, like viewing what's in a folder, and add symbols to identify what each item is.
 ls -F
+# Update the list of available packages
+sudo apt update
+# Update the programs you already have installed
+sudo apt upgrade
+# Install the parted tool to manage disks
+sudo apt install parted
