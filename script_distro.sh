9a673d0c1aa6a514db8ba15b109f845f2be51b37
Kevin <132043896+kkevincruzattjw@users.noreply.github.com>
2026-04-10 17:40:09 +0000

    make -j 2

--- script_distro.sh
+++ script_distro.sh
@@ -12,3 +12,4 @@ cd .. #This is used to move you to the parent directory of the current one.
 git clone --depth 1 https://git.busybox.net/busybox 
 cd busybox #It is used to clone the official BusyBox repository with minimal history and enter its folder.
 make menuconfig #In BusyBox, this is used to customize which commands and utilities will be available in your BusyBox executable.
+make -j 2 #generates the main busybox binary using two parallel processes.
