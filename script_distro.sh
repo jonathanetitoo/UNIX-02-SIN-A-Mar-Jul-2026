08cbd779c45a58e0eace788ed959246361defff0
Kevin <132043896+kkevincruzattjw@users.noreply.github.com>
2026-04-10 16:44:05 +0000

    make menuconfig

--- script_distro.sh
+++ script_distro.sh
@@ -3,3 +3,4 @@ sudo apt install -y git vim make gcc libncurses-dev flex bison bc \
   cpio libelf-dev libssl-dev syslinux dosfstools qemu-system-x86 #This command is used to automatically install Git, Vim, compilers, libraries, and tools necessary for development and compilation on Linux.
 git clone --depth 1 https://github.com/torvalds/linux.git 
 cd linux #This set of commands is used to clone the official Linux repository and enter its folder.
+make menuconfig #It is used to open an interactive menu to configure kernel options before compiling.
