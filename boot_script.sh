f29612d118cffc7cb9cf9bf945005b86db6a01d8
Kevin <132043896+kkevincruzattjw@users.noreply.github.com>
2026-04-16 12:54:19 +0000

    mkdir -- -rf

--- boot_script.sh
+++ boot_script.sh
@@ -4,3 +4,4 @@ ls -l #listar directorios y archivos que tengo
 ls -h #mostrar tamaños de archivos en formato legible (KB, MB, GB) en la lista de ls
 ls -l -a -h #Cuando los combinas en ls -lah, obtienes todo junto: lista detallada + archivos ocultos + tamaños legibles.
 ls -lah #muestra todo, con detalles y tamaños fáciles de leer.
+mkdir -- -rf #irve para proteger nombres de archivos o carpetas que empiezan con guion (-), evitando que se confundan con parámetros del comando.
