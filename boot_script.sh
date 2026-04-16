45a2d9453bd115e653b0bcbf7936078ac1c864e7
Kevin <132043896+kkevincruzattjw@users.noreply.github.com>
2026-04-16 12:48:15 +0000

    ls -h

--- boot_script.sh
+++ boot_script.sh
@@ -1,3 +1,4 @@
 ls -a #opciones cortas
 ls -all #Forma corta que hace lo mismo de ls -a, ls -all muestra hasta ocultos
 ls -l #listar directorios y archivos que tengo
+ls -h #mostrar tamaños de archivos en formato legible (KB, MB, GB) en la lista de ls
