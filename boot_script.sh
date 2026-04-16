c8bec2d56fce7f01a21fd4aa4647dcd947c7ce90
Kevin <132043896+kkevincruzattjw@users.noreply.github.com>
2026-04-16 12:48:51 +0000

    ls -lah

--- boot_script.sh
+++ boot_script.sh
@@ -3,3 +3,4 @@ ls -all #Forma corta que hace lo mismo de ls -a, ls -all muestra hasta ocultos
 ls -l #listar directorios y archivos que tengo
 ls -h #mostrar tamaños de archivos en formato legible (KB, MB, GB) en la lista de ls
 ls -l -a -h #Cuando los combinas en ls -lah, obtienes todo junto: lista detallada + archivos ocultos + tamaños legibles.
+ls -lah #muestra todo, con detalles y tamaños fáciles de leer.
