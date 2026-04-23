d1d3e5dd521f912eb50c06addc35ee1aa8353867
Kevin <132043896+kkevincruzattjw@users.noreply.github.com>
2026-04-23 12:40:45 +0000

    sudo -i

--- boot_script.sh
+++ boot_script.sh
@@ -39,3 +39,4 @@ confirmar #Esto solo lo hago para confirmar que estoy guardando correctamente
 
 sudo sh -c 'echo "chao" >> /etc/archivo_protegido' # Add the word "chao" to the end of a protected system file using administrator privileges
 cat /etc/archivo_protegido # Display the contents of the protected file directly on your screen.
+sudo -i # Open an interactive session as superuser (root), turning your terminal into a full administrator console.
