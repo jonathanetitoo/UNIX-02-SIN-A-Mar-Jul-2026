731d05c04cab6744ee769bad0df4ecdb537b696b
ashweeps <asespinozaro@uide.edu.ec>
2026-05-02 17:27:49 +0000

    ls-l and ls-r

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,4 +1,8 @@
 #The command ls is used to see the files and folders in a directory. It shows a list of everything inside the current folder.
 ls 
-#
-aptitude moo
+#It is not a serious command. It is just a joke inside the system, and it prints a cow saying “moo”.
+apt moo
+#Lists all files and folders with detailed information like size, permissions, owner, and date of modification.
+ls -l
+#Lists files and folders in reverse order, showing the last items first instead of the normal order.
+ls -r
