b7534100d4205e0a4421e0e337fb8d9e449817ee
Dav1dVaca <davacagu@uide.edu.ec>
2026-05-12 17:19:06 +0000

    create/ change owner

--- security.zsh
+++ security.zsh
@@ -7,3 +7,9 @@ sudo apt upgrade            # Install available updates for all current packages
 sudo apt install acl       # Install the Access Control List management utility
 chown -R $(whoami) .       # Give ownership of current folder to the active user
 setfacl -bnR .             # Remove all extended ACL entries recursively
+umask 022                  # Set default permissions to 755 (rwxr-xr-x)
+whoami                     # Display the current active username
+echo "Hola" > mi_archivo   # Create "mi_archivo" with the text "Hola"
+ls -l mi_archivo           # List details and permissions of "mi_archivo"
+useradd -m -s /usr/bin/zsh david  # Create user "david" with home dir and ZSH shell
+chown david mi_archivo     # Change the owner of "mi_archivo" to user "david"
